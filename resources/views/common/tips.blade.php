4d3c92a012d8c2e424712b02714d4d1c73689c8a
Maple.Leaf <kfbb520@gmail.com>
2020-09-18 17:25:09 +0800

    打赏功能

---
+++ resources/views/common/tips.blade.php
@@ -0,0 +1,13 @@
+<link rel="stylesheet" type="text/css" href="{{ asset('common/css/tips.css') }}">
+<div class="qrTips">
+    <div id="DonateText" class="tr3">Sponsor</div>
+    <ul id="donateBox" class="list tr3">
+        <li id="Tips">打赏一下</li>
+        <li id="AliPay">AliPay</li>
+        <li id="WeChat">WeChat</li>
+    </ul>
+    <div id="QRBox">
+        <div id="MainBox"></div>
+    </div>
+</div>
+<script async src="{{ asset('common/js/tips.js') }}"></script>
--- resources/views/showTime/blog/info.blade.php
+++ resources/views/showTime/blog/info.blade.php
@@ -33,15 +33,7 @@
                 </div>
             </div>
             <div class="share">
-                <!-- 代码1：放在页面需要展示的位置  -->
-                <!-- 如果您配置过sourceid，建议在div标签中配置sourceid、cid(分类id)，没有请忽略  -->
-                <div id="cyReward" role="cylabs" data-use="reward" sourceid="{{ $info->id }}"></div>
-                <!-- 代码2：用来读取评论框配置，此代码需放置在代码1之后。 -->
-                <!-- 如果当前页面有评论框，代码2请勿放置在评论框代码之前。 -->
-                <!-- 如果页面同时使用多个实验室项目，以下代码只需要引入一次，只配置上面的div标签即可 -->
-                <script type="text/javascript" charset="utf-8" src="https://changyan.itc.cn/js/lib/jquery.js"></script>
-                <script type="text/javascript" charset="utf-8"
-                        src="https://changyan.sohu.com/js/changyan.labs.https.js?appid=cytXTxfaG"></script>
+                @include('common/tips')
             </div>
             <div class="social-share share" data-title="{{ $info->title }}"
                  data-description="{{ $info->summary or $info->title }}"
@@ -114,7 +106,7 @@
                 @foreach($featured as $feat)
                     <li>
                         <a href="{{ route("photo", $feat->travelId) }}">
-                            <img src="{{ $feat->img }}" alt="秋枫阁-{{ $feat->img }}"/></a>
+                            <img src="{{ $feat->img }}" alt="秋枫阁-{{ $feat->img }}"/>
                         </a>
                     </li>
                 @endforeach
