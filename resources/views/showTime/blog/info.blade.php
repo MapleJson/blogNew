0eed1a59c4c7f36c6f253ce6022e6e05f6497707
Maple.Leaf <kfbb520@gmail.com>
2021-08-10 10:34:21 +0800

    message

--- resources/views/showTime/blog/info.blade.php
+++ resources/views/showTime/blog/info.blade.php
@@ -54,46 +54,46 @@
                 @endif
             </div>
 
-            <div class="news_pl">
-                <h2>文章评论</h2>
-                <!--PC和WAP自适应版-->
-                <div id="SOHUCS" sid="{{ $info->id }}"></div>
-                <script type="text/javascript">
-                    (function () {
-                        var appid = 'cytXTxfaG';
-                        var conf = 'prod_d2b1775335de7bd9a7534ed2c36d07ef';
-                        var width = window.innerWidth || document.documentElement.clientWidth;
-                        if (width < 960) {
-                            window.document.write('<script id="changyan_mobile_js" charset="utf-8" type="text/javascript" src="http://changyan.sohu.com/upload/mobile/wap-js/changyan_mobile.js?client_id=' + appid + '&conf=' + conf + '"><\/script>');
-                        } else {
-                            var loadJs = function (d, a) {
-                                var c = document.getElementsByTagName("head")[0] || document.head || document.documentElement;
-                                var b = document.createElement("script");
-                                b.setAttribute("type", "text/javascript");
-                                b.setAttribute("charset", "UTF-8");
-                                b.setAttribute("src", d);
-                                if (typeof a === "function") {
-                                    if (window.attachEvent) {
-                                        b.onreadystatechange = function () {
-                                            var e = b.readyState;
-                                            if (e === "loaded" || e === "complete") {
-                                                b.onreadystatechange = null;
-                                                a()
-                                            }
-                                        }
-                                    } else {
-                                        b.onload = a
-                                    }
-                                }
-                                c.appendChild(b)
-                            };
-                            loadJs("http://changyan.sohu.com/upload/changyan.js", function () {
-                                window.changyan.api.config({appid: appid, conf: conf})
-                            });
-                        }
-                    })();
-                </script>
-            </div>
+{{--            <div class="news_pl">--}}
+{{--                <h2>文章评论</h2>--}}
+{{--                <!--PC和WAP自适应版-->--}}
+{{--                <div id="SOHUCS" sid="{{ $info->id }}"></div>--}}
+{{--                <script type="text/javascript">--}}
+{{--                    (function () {--}}
+{{--                        var appid = 'cytXTxfaG';--}}
+{{--                        var conf = 'prod_d2b1775335de7bd9a7534ed2c36d07ef';--}}
+{{--                        var width = window.innerWidth || document.documentElement.clientWidth;--}}
+{{--                        if (width < 960) {--}}
+{{--                            window.document.write('<script id="changyan_mobile_js" charset="utf-8" type="text/javascript" src="http://changyan.sohu.com/upload/mobile/wap-js/changyan_mobile.js?client_id=' + appid + '&conf=' + conf + '"><\/script>');--}}
+{{--                        } else {--}}
+{{--                            var loadJs = function (d, a) {--}}
+{{--                                var c = document.getElementsByTagName("head")[0] || document.head || document.documentElement;--}}
+{{--                                var b = document.createElement("script");--}}
+{{--                                b.setAttribute("type", "text/javascript");--}}
+{{--                                b.setAttribute("charset", "UTF-8");--}}
+{{--                                b.setAttribute("src", d);--}}
+{{--                                if (typeof a === "function") {--}}
+{{--                                    if (window.attachEvent) {--}}
+{{--                                        b.onreadystatechange = function () {--}}
+{{--                                            var e = b.readyState;--}}
+{{--                                            if (e === "loaded" || e === "complete") {--}}
+{{--                                                b.onreadystatechange = null;--}}
+{{--                                                a()--}}
+{{--                                            }--}}
+{{--                                        }--}}
+{{--                                    } else {--}}
+{{--                                        b.onload = a--}}
+{{--                                    }--}}
+{{--                                }--}}
+{{--                                c.appendChild(b)--}}
+{{--                            };--}}
+{{--                            loadJs("http://changyan.sohu.com/upload/changyan.js", function () {--}}
+{{--                                window.changyan.api.config({appid: appid, conf: conf})--}}
+{{--                            });--}}
+{{--                        }--}}
+{{--                    })();--}}
+{{--                </script>--}}
+{{--            </div>--}}
         </div>
     </main>
 @stop
